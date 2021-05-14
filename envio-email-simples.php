90d3917a487f28716d978e74034e19a5b7f75bcb
Janderson Garcoa <70637518+jandersongarcia@users.noreply.github.com>
2021-05-14 09:48:10 -0400

    Update envio-email-simples.php

--- envio-email-simples.php
+++ envio-email-simples.php
@@ -1,7 +1,7 @@
 <?php
-	$para = ‘contato@jandersongarcia.com.br’;
-	$assunto = ‘Enviando e-mail com o PHP’;
-	$mensagem = ‘Olá, este é um e-mail teste.’;
+	$para = "contato@jandersongarcia.com.br";
+	$assunto = "Enviando e-mail com o PHP";
+	$mensagem = "Olá, este é um e-mail teste.";
 	if(mail($para,$assunto,$mensagem)){
 		echo "Mensagem enviada com sucesso.";
 	} else {
